0d3b1ef2ca8cc719b5ede5da5caa92baf1a2b86a
Orlanesthesie <orlane.r.6@gmail.com>
2024-03-05 11:58:13 +0100

    deuxieme push

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,44 @@
+<footer class="bg-grey-dark">
+    <div class="container">
+        <img src="../assets/images/logo/Travel_Logo.png" class="mx-auto d-block align-items-center justify-content-center" alt="">
+        <p class="grey-light text-center fw-bold">Comparateur de tours opérateur spécialisé dans les séjours, les circuits, les autotours et les vacances en Hotels Club.<br>
+            TIME TRAVEL - 55 Rue du Faubourg Saint-Honoré, 75008 Paris - SAS au Capital de 450 000€ - RCS Antibes 922 428 495<br>
+            www.timetravel.com<br>
+            Immatriculation opérateur de voyage N°463537453<br>
+            TIMETRAVEL est un marque de SVC VOYAGE SAS</p>
+        <hr class="text-white">
+        <h3 class="fw-bold text-white text-center"> NOS SERVICES </h3>
+        <ul class="text-white fw-bold text-center" style="list-style-type:none;">
+            <li>Sécurité</li>
+            <li>Tous nos départs</li>
+            <li>Nos tours opérateurs</li>
+            <li>Assurances</li>
+            <li>Mode d’emploi</li>
+            <li>Emploi</li>
+            <li>Conditions générale de vente</li>
+            <li>Plan de site </li>
+        </ul>
+        <hr class="text-white">
+        <h3 class="fw-bold text-white text-center"> SÉJOURS VOYAGES </h3>
+        <ul class="text-white fw-bold text-center" style="list-style-type:none;">
+            <li>Qui sommes-nous ?</li>
+            <li>Contactez vous</li>
+        </ul>
+        <div class="p-3 text-center">
+            <i class="fa-brands fa-instagram fa-2xl p-2" style="color: #000000;"></i>
+            <i class="fa-brands fa-x-twitter fa-2xl" style="color: #000000;"></i>
+            <i class="fa-brands fa-facebook fa-2xl p-2" style="color: #000000;"></i>
+        </div>
+
+        <h5 class="fw-bold grey-light text-center mt-3">© TimeTravelCorp </h5>
+
+    </div>
+
+
+
+
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
